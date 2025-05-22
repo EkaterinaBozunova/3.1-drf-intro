3d8bad29f28fde1f976dbd6f6e1decbf340b3b33
Ekaterina.Bozunova <katrin060189@gmail.com>
2025-05-22 23:52:22 +0700

    refactoring views

--- smart_home/measurement/views.py
+++ smart_home/measurement/views.py
@@ -1,11 +1,19 @@
 from rest_framework import generics
-from .models import ViewModel
-from .serializers import ViewSerializer
+from .models import Sensor, Measurement
+from .serializers import SensorSerializer, MeasurementSerializer
 
-class ViewListCreate(generics.ListCreateAPIView):
-    queryset = ViewModel.objects.all()
-    serializer_class = ViewSerializer
+# Для работы с списком и созданием датчиков
+class SensorListCreateView(generics.ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
 
-class ViewRetrieveUpdate(generics.RetrieveUpdateAPIView):
-    queryset = ViewModel.objects.all()
-    serializer_class = ViewSerializer
+# Для получения, редактирования и удаления конкретного датчика
+class SensorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+    lookup_field = 'id'
+
+# Для создания новых измерений
+class MeasurementCreateView(generics.CreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
